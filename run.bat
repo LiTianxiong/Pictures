0dab5e741709d7152100815e8138df1d3c456ab5
litmxs <litmxs@gmail.com>
2019-05-09 12:23:13 +0800

    upload

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
+if "%1"=="hide" goto CmdBegin
+start mshta vbscript:createobject("wscript.shell").run("""%~0"" hide",0)(window.close)&&exit
+:CmdBegin
 git add *
 git commit -m "upload"
 git push origin master
